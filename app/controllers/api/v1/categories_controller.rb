f2f0f4a473bbbd7c2d41a503d22589e62f5518c8
“Hellionfrs” <fredy.rodriguez@pucp.pe>
2022-11-09 14:41:25 -0500

    Categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,3 +1,16 @@
-class Api::V1:CategoriesController < ApplicationController
-  
+class Api::V1::CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+  def index
+    @categories = Category.all
+    # render json: {products: Product.all}
+    render json: @categories
+  end
+
+  def show 
+    render json: @product
+  end
+
+  def set_category
+    @product = Category.find(params[:id])
+  end
 end
